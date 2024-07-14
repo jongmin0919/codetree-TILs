f8de1aedd5f147b317ed004b733567901c276a54
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-15 08:44:50 +0900

    Title: 문자열 출력하기, Time: 217ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240715/문자열 출력하기/print-string.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const n = parseInt(input);
+
+for(let i = 0; i < n ; i++){
+    console.log('LeebrosCode')
+}
