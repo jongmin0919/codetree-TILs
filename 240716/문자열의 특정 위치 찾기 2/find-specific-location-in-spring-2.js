18c493de6931f43380db6cf5688e8e06b947e569
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-16 08:51:54 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

--- 240716/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.js
+++ 240716/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.js
@@ -9,8 +9,13 @@ let stack = [];
 for(let i = 0; i < arr.length ; i++){
     if(arr[i][2] === input || arr[i][3] === input){
         stack.push(arr[i])
+        console.log(arr[i])
     } 
 }
 
-stack.forEach(item => console.log(item));
+/* 
+    foreach로 돌려도 되긴 하는데 애초에 윗쪽 for문에서 출력한 후 사이즈만 출력해 줘도 됨.
+    stack.forEach(item => console.log(item));
+*/
+
 console.log(stack.length);
