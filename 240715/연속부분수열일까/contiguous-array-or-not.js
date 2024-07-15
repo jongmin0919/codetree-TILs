c5800996f5dc9d5989bb2834c09021ae389082ff
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-15 10:55:38 +0900

    Title: 연속부분수열일까, Time: 166ms, Memory: 14MB, Status: Passed - CodeTree

--- 240715/연속부분수열일까/contiguous-array-or-not.js
+++ 240715/연속부분수열일까/contiguous-array-or-not.js
@@ -7,33 +7,24 @@ const numberArr_A = input[1].split(" ").map(Number);
 const numberArr_B = input[2].split(" ").map(Number);
 
 let count = 0;
-if(aN > bN){
+if (aN >= bN) {
     let idx = 0;
-    for(let i = 0 ; i < aN ; i++){
-        if(numberArr_A[i] === numberArr_B[idx]){
+    for (let i = 0; i < aN; i++) {
+        if (numberArr_A[i] === numberArr_B[idx]) {
             count++;
             idx++;
-            if(count === bN){
-              onsole.log('Yes')
-              break;  
+            if (count === bN) {
+                console.log('Yes');
+                break;
             }
-        }else{
-            idx = 0
+        } else {
+            idx = 0;
             count = 0;
         }
     }
-    if(count !== bN) console.log('No')
-}else if(aN === bn){
-    for(let i = 0; i < aN ; i++){
-        if(numberArr_A[i] === numberArr_B[i]){
-            count++;
-            if(count === aN){
-              console.log('Yes')
-              break;  
-            } 
-        }else{
-            console.log('No')
-            break;
-        }
+    if (count !== bN) {
+        console.log('No');
     }
+} else {
+    console.log('No');
 }
