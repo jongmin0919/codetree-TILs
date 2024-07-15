8d312b4beff8620fa7e325461da228c0cad8dbf9
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-15 10:52:12 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/연속부분수열일까/contiguous-array-or-not.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const firstLine = input[0].split(" ");
+const aN = parseInt(firstLine[0]);
+const bN = parseInt(firstLine[1]);
+const numberArr_A = input[1].split(" ").map(Number);
+const numberArr_B = input[2].split(" ").map(Number);
+
+let count = 0;
+if(aN > bN){
+    let idx = 0;
+    for(let i = 0 ; i < aN ; i++){
+        if(numberArr_A[i] === numberArr_B[idx]){
+            count++;
+            idx++;
+            if(count === bN){
+              onsole.log('Yes')
+              break;  
+            }
+        }else{
+            idx = 0
+            count = 0;
+        }
+    }
+    if(count !== bN) console.log('No')
+}else if(aN < bn){
+    let idx = 0;
+    for(let i = 0 ; i < bN ; i++){
+        if(numberArr_A[i] === numberArr_B[idx]){
+            count++;
+            idx++;
+            if(count === aN){
+              console.log('Yes')
+              break;  
+            } 
+        }else{
+            idx = 0
+            count = 0;
+        }
+    }
+    if(count !== bN) console.log('No')
+}else{
+    for(let i = 0; i < aN ; i++){
+        if(numberArr_A[i] === numberArr_B[i]){
+            count++;
+            if(count === aN){
+              console.log('Yes')
+              break;  
+            } 
+        }else{
+            console.log('No')
+            break;
+        }
+    }
+}
