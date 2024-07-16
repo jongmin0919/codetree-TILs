abc3b4402bb7b146aef50cb5a95551c0043ef0b4
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-16 09:09:36 +0900

    Title: 문자열의 총 길이 구하기, Time: 174ms, Memory: 47MB, Status: Passed - CodeTree

--- 240716/문자열의 총 길이 구하기/find-the-length-of-the-string.js
+++ 240716/문자열의 총 길이 구하기/find-the-length-of-the-string.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 
-let arr = [...input];
+
 let cnt = 0;
 
+// 입력을 배열로 변환 후 foreach를 이용해 각 문자열의 길이를 cnt에 누적 후 출력
+let arr = [...input];
 arr.forEach(item => cnt += item.length);
 
 console.log(cnt);
