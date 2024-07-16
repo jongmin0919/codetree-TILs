08b8f6e9552b232b17a96cd068d73617a592b481
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-16 16:41:52 +0900

    Title: 정수의 최솟값, Time: 229ms, Memory: 14MB, Status: Passed - CodeTree

--- 240716/정수의 최솟값/minimum-value-of-an-integer.js
+++ 240716/정수의 최솟값/minimum-value-of-an-integer.js
@@ -1,15 +1,12 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(" ");
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-
-
-function printMinimalNumber(str){
-    let arr = [...str];
+function printMinimalNumber(arr){
     let min = arr[0];
-    for(let i = 0; i < str.length ; i++){
+    for(let i = 1; i < arr.length; i++){
         if(arr[i] < min) min = arr[i];
     }
     console.log(min);
 }
 
-printMinimalNumber(input)
+printMinimalNumber(input);
