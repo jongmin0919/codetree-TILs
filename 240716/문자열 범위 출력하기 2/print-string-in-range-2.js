e6bedcf4f8e48dfcfde5ee9a93c5ff8a0959b3ab
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-16 15:28:46 +0900

    Title: 문자열 범위 출력하기 2, Time: 166ms, Memory: 14MB, Status: Passed - CodeTree

--- 240716/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 240716/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -4,6 +4,7 @@ let letter = givenStr[0]
 let length = parseInt(givenStr[1]);
 
 let result = "";
+// 주어진 숫자보다 문자열길이가 길거나 같으면 뒤에서부터 역순으로 차례대로 출력하고, 주어진 숫자가 더 크면 뒤부터 몽땅 출력.
 if(length <= letter.length){
     for(let i = (letter.length) - 1  ; i > (letter.length - length) -1 ; i--){
         result += letter[i];
