660749d452f5381833df40e7105eaed46c91b3c5
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-17 22:04:57 +0900

    Title: 부분문자열 위치 구하기, Time: 312ms, Memory: 14MB, Status: Passed - CodeTree

--- 240717/부분문자열 위치 구하기/find-location-of-substring.js
+++ 240717/부분문자열 위치 구하기/find-location-of-substring.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let [letterA, letterB] = [input[0], input[1]]
+let [letterA, letterB] = [input[0], input[1]];
 
-let result = 0;
-for(let i = 0 ; i < letterA.length ; i++){
-    if(letterA[i] === letterB[0]){
+let result = -1;
+for (let i = 0; i < letterA.length; i++) {
+    if (letterA[i] === letterB[0]) {
         let flag = true;
-        for(let j = i; j < letterB.length + i ; j++){
-            if(letterA[j] !== letterB[j]) false;
+        for (let j = 0; j < letterB.length; j++) {
+            if (letterA[i + j] !== letterB[j]) {
+                flag = false;
+                break;
+            }
         }
-        if(flag){
-            result = i
+        if (flag) {
+            result = i;
             break;
-        }else{
-            result = -1
-            continue;
         }
     }
 }
 
-console.log(result ? result : -1);
+console.log(result);
