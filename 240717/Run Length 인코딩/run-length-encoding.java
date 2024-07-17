96a8fdf656e7f35d1b638f31ffd4943f0978c1fe
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-17 23:31:00 +0900

    Title: Run Length 인코딩, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240717/Run Length 인코딩/run-length-encoding.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine().trim();
+
+        List<Character> alphaStack = new ArrayList<>();
+        List<Integer> encodingStack = new ArrayList<>();
+
+        char state = input.charAt(0);
+        alphaStack.add(state);
+        int count = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (state == input.charAt(i)) {
+                count++;
+            } else {
+                encodingStack.add(count);
+                state = input[.charAt(i)];
+                alphaStack.add(state);
+                count = 1;
+            }
+        }
+        encodingStack.add(count); // 마지막 문자의 등장 횟수 추가
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < alphaStack.size(); i++) {
+            result.append(alphaStack.get(i)).append(encodingStack.get(i));
+        }
+
+        System.out.println(result.length());
+        System.out.println(result);
+    }
+}
