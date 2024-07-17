6a9e43642f74b39b317fcbc82cc073c1dbd8fa45
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-17 22:43:01 +0900

    Title: 함수를 이용한 소수 판별, Time: 237ms, Memory: 15MB, Status: Passed - CodeTree

--- 240717/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
+++ 240717/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -4,6 +4,8 @@ let [a,b] = [parseInt(input[0]), parseInt(input[1])]
 
 function isPrime(num){
     let result = num;
+    if (num <= 1) return 0; // 1 이하의 숫자는 소수가 아님
+    if (num === 2) return num; // 2는 소수
     for(let i = 2 ; i < num ; i++){
         if(num%i === 0){
             result = 0;
