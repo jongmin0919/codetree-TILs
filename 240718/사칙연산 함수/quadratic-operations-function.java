643daf44d208223e591a1e6b9a936aa073d3f8dc
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-18 09:44:53 +0900

    Title: 사칙연산 함수, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

--- 240718/사칙연산 함수/quadratic-operations-function.java
+++ 240718/사칙연산 함수/quadratic-operations-function.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 
 public class Main {
 
-    // 사칙연산 오버라이딩
+    // 사칙연산 오버라이딩 (나누기도 정수가 나와야 되므로 나누기에 대한 오버라이딩은 따로 하지 않음)
      public static int calculator(int a, int b, char operator){
         if(operator == '+') return a + b;
         else if(operator == '-') return a - b;
         else if(operator == '*') return a * b;
         else if(operator == '/') return a / b;
-        else return false; 
+        else return 0; 
     }
 
     public static void main(String[] args) throws IOException{
@@ -21,11 +21,9 @@ public class Main {
         int b = Integer.parseInt(arr[2]);
         char o = arr[1].charAt(0);
         
-        if(calculator(a,b,o) == false){
-            System.out.printf(calculator(a,b,o));        
-        }else{
-            System.out.printf(a + " " + o + " " + b + " = " + calculator(a,b,o));    
-        }
+        if(calculator(a,b,o) == 0) System.out.println("False");        
+        else System.out.printf(a + " " + o + " " + b + " = " + calculator(a,b,o));    
+        
         
         
             
