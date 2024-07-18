6570d7dfb8f226aca9bcb33553d1e8521f7dc168
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-18 14:58:43 +0900

    Title: 그 계절, 그 날, Time: 111ms, Memory: 8MB, Status: Passed - CodeTree

--- 240718/그 계절, 그 날/that-season-that-day.java
+++ 240718/그 계절, 그 날/that-season-that-day.java
@@ -8,16 +8,19 @@ public class Main {
     static int year = 0;
     static boolean flag = true;
 
+    // 평년일 때 체크하는 메서드
     public static String checkNormalYear(int month, int day) {
         flag = true;
         Main.month = 0;
 
+        // 유효한 달인지 판단
         if (month >= 1 && month <= 12) {
             Main.month = month;
         } else {
             return "-1";
         }
         
+        // 유효한 날짜인지 판단
         switch (Main.month) {
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 if (day < 1 || day > 31) flag = false;
@@ -30,6 +33,7 @@ public class Main {
                 break;
         }
 
+        // 계절 서칭
         if(flag){
             if(month >= 3 && month <= 5) return "Spring";
             else if(month >= 6 && month <= 8) return "Summer";
@@ -39,6 +43,7 @@ public class Main {
         return "-1"; 
     }
 
+    // 윤년일 때 체킹 (이하 유효)
     public static String checkLeapYear(int month, int day) {
         flag = true;
         Main.month = 0;
@@ -70,19 +75,22 @@ public class Main {
         return "-1";
     }
 
+    // 주어진 연도를 기준으로 윤년, 평년 메서드 중 조건에 따라 메서드를 하나 실행하는 코드
     public static String checkYearIs(int year, int month, int day) {
+        // 400배수면 윤년 메서드 실행
         if (year % 400 == 0) {
             return checkLeapYear(month, day);
+        // 100 배수면 평년 메서드 실행
         } else if (year % 100 == 0) {
             return checkNormalYear(month, day);
-        } else if (year % 4 == 0) {
+        // 4의 배수면서 100의 배수가 아니거나, 400의 배수면 윤년 메서드 실행
+        } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             return checkLeapYear(month, day);
         } else {
             return checkNormalYear(month, day);
         }
     }
     
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = br.readLine().split(" ");
