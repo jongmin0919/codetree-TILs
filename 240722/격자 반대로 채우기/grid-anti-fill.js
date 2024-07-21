3c884c008aec37d0af574d3ecb332eebaf307444
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-22 01:54:10 +0900

    Title: 격자 반대로 채우기, Time: 164ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240722/격자 반대로 채우기/grid-anti-fill.js
+++ 240722/격자 반대로 채우기/grid-anti-fill.js
@@ -3,24 +3,24 @@ const input = parseInt(fs.readFileSync('/dev/stdin').toString());
 
 const n = input;
 
-let arr2d = Array(n).fill(0).map(()=> Array(n).fill(0));
+let arr2d = Array.from({ length: n }, () => Array(n).fill(0));
 
 let num = 1;
-for(let i = n-1 ; i >= 0 ; i--){
-    if(i%2 === 0){
-        for(let j = 0 ; j < n ; j++){
+for (let i = n - 1; i >= 0; i--) {
+    if (i % 2 === 0) {
+        for (let j = 0; j < n; j++) {
             arr2d[j][i] = num++;
         }
-    }else{
-        for(let j = n-1 ; j >= 0 ; j--){
+    } else {
+        for (let j = n - 1; j >= 0; j--) {
             arr2d[j][i] = num++;
         }
     }
 }
 
-for(let arr of arr2d){
+for (let arr of arr2d) {
     let str = "";
-    for(item of arr){
+    for (let item of arr) {
         str += item + " ";
     }
     console.log(str.trim());
