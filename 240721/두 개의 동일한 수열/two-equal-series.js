173e5eef5e0b10a0d34b6946e1e8335592078754
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-21 17:16:09 +0900

    Title: 두 개의 동일한 수열, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

--- 240721/두 개의 동일한 수열/two-equal-series.js
+++ 240721/두 개의 동일한 수열/two-equal-series.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const length = parseInt(input[0].split(" ")[0]);
+// 뭔지는 모르겠지만 split을 실행하면 없던 0이 생겨버림. 그래서 filter로 false(0) 값을 제거함. 
+// 조건도 원소의 값은 1부터 100까지이니 0은 없어도 됨.
+let numbersA = input[1].split(" ").filter(n => n).map(Number).sort((a, b) => a - b);
+let numbersB = input[2].split(" ").filter(n => n).map(Number).sort((a, b) => a - b);
 
-let numbersA = input[1].split(" ").map(Number).sort((a,b) => a-b);
-let numbersB = input[2].split(" ").map(Number).sort((a,b) => a-b);
+let flag = true;
+if (numbersA.length !== numbersB.length) {
+    flag = false;
+} else {
+    numbersA.forEach((num, idx) => {
+        if (num !== numbersB[idx]) {
+            flag = false;
+        }
+    });
+}
 
-console.log(numbersA.join(" ") === numbersB.join(" ") ? "Yes" : "No");
+console.log(flag ? "Yes" : "No");
