531d4b5834c0eca9166b9f19a9bc57c2645a90b5
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-21 21:33:39 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

--- 240721/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
+++ 240721/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
@@ -2,16 +2,28 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let arr = input[1].split(" ").map(Number);
-let acc = 0;
 
-let maxStack = [];
-for(let i = 0 ; i < arr.length-1 ; i++){
-    if(arr[i] - arr[i+1] < 0){
-        
-        acc += Math.abs(arr[i] - arr[i+1]);
-        maxStack.push(acc);
-    }else acc = 0;
+let minPrice = arr[0]; 
+let maxProfit = 0; 
+
+for (let i = 1; i < arr.length; i++) {
+    const currentPrice = arr[i];
+    
+    // 현재 가격에서 최소 가격을 뺀 이익을 계산하고
+    const profit = currentPrice - minPrice;
+    
+    // 현재 이윤이 최대 이윤보다 높을 경우 갈아 치움
+    if (profit > maxProfit) {
+        maxProfit = profit;
+    }
+    
+    /* 현재 가격이 최소 가격보다 낮은 경우, 
+    그러니까 가장 낮은 최소 가격에서 현재 가격을 뺐을 때 최대 이윤을 얻어야 하니까 
+    최소 가격을 최대한 작게 정해야함
+    */
+    if (currentPrice < minPrice) {
+        minPrice = currentPrice;
+    }
 }
-let maxEarnings = Math.max(...maxStack)
 
-console.log(maxEarnings > 0 ? maxEarnings : 0);
+console.log(maxProfit);
