42fd025fbb7f11c68b15ab71e14d71ba20ff091e
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-22 16:17:11 +0900

    Title: 격자로 사각형 만들기, Time: 193ms, Memory: 15MB, Status: Passed - CodeTree

--- 240722/격자로 사각형 만들기/print-grid-in-rectangle.js
+++ 240722/격자로 사각형 만들기/print-grid-in-rectangle.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = parseInt(fs.readFileSync('/dev/stdin').toString());
+const input = parseInt(fs.readFileSync('/dev/stdin').toString().trim(), 10);
 
 let arr2d = Array.from({ length: input }, () => Array(input).fill(0));
 
@@ -9,16 +9,18 @@ function printArr(arr){
         for (let elem of row) {
             str += elem + " ";
         }
-        console.log(str);
+        console.log(str.trim());
     }
 }
 
-
-for(let i = 0 ; i < 5 ; i++){
-    for(let j = 0 ; j < 5 ; j++){
-        if(i == 0 || j == 0) arr2d[i][j] = 1
-        else arr2d[i][j] = arr2d[i-1][j] + arr2d[i][j-1] + arr2d[i-1][j-1]
+for(let i = 0 ; i < input ; i++){
+    for(let j = 0 ; j < input ; j++){
+        if(i == 0 || j == 0) {
+            arr2d[i][j] = 1;
+        } else {
+            arr2d[i][j] = arr2d[i-1][j] + arr2d[i][j-1] + arr2d[i-1][j-1];
+        }
     }
 }
 
-printArr(arr2d)
+printArr(arr2d);
