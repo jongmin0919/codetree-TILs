0adb86808ee369f54c52cefe6cb8abae665b3475
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-22 16:15:18 +0900

    Title: 격자로 사각형 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/격자로 사각형 만들기/print-grid-in-rectangle.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = parseInt(fs.readFileSync('/dev/stdin').toString());
+
+let arr2d = Array.from({ length: input }, () => Array(input).fill(0));
+
+function printArr(arr){
+    for (let row of arr) {
+        let str = "";
+        for (let elem of row) {
+            str += elem + " ";
+        }
+        console.log(str);
+    }
+}
+
+let num = 1;
+for(let i = 0 ; i < 5 ; i++){
+    for(let j = 0 ; j < 5 ; j++){
+        if(i == 0 || j == 0) arr2d[i][j] = 1
+        else arr2d[i][j] = arr2d[i-1][j] + arr2d[i][j-1] + arr2d[i-1][j-1]
+    }
+}
+
+printArr(arr2d)
