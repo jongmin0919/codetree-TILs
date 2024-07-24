3d4430b87ca8b1d0299aa376d627ae206f9ebea0
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-24 11:30:20 +0900

    Title: 2개씩 그룹짓기, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

--- 240724/2개씩 그룹짓기/group-of-pairs.js
+++ 240724/2개씩 그룹짓기/group-of-pairs.js
@@ -1,50 +1,21 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
-const length = parseInt(input[0]);
-let group = input[1].split(" ").map(Number);
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// 이분 탐색과 그리디 검사를 위한 함수 정의
-function canFormGroups(nums, maxSum) {
-    let used = new Array(nums.length).fill(false);
+// 변수 선언 및 입력
+const n = Number(input[0]);
+const nums = input[1].trim().split(' ').map(Number);
 
-    function dfs(index, currentSum) {
-        if (index === nums.length) {
-            return true;
-        }
-        if (used[index]) {
-            return dfs(index + 1, currentSum);
-        }
-        for (let i = index + 1; i < nums.length; i++) {
-            if (!used[i] && currentSum + nums[i] <= maxSum) {
-                used[i] = true;
-                if (dfs(index + 1, currentSum + nums[i])) {
-                    return true;
-                }
-                used[i] = false;
-            }
-        }
-        return false;
+// nums를 정렬합니다.
+nums.sort((a, b) => a - b);
+
+let group_max = 0;
+for (let i = 0; i < n; i++) {
+    // i번째와 2n - 1 - i번째 원소를 매칭합니다.
+    const group_sum = nums[i] + nums[2 * n - 1 - i];
+    if (group_sum > group_max) {
+        // 최댓값을 갱신합니다.
+        group_max = group_sum;
     }
-    
-    return dfs(0, 0);
 }
 
-function minimizeMaxGroupSum(nums) {
-    nums.sort((a, b) => a - b);
-    let low = nums[nums.length - 1];
-    let high = nums.reduce((acc, num) => acc + num, 0);
-    
-    while (low < high) {
-        let mid = Math.floor((low + high) / 2);
-        if (canFormGroups(nums, mid)) {
-            high = mid;
-        } else {
-            low = mid + 1;
-        }
-    }
-    
-    return low;
-}
-
-// 실행 및 결과 출력
-console.log(minimizeMaxGroupSum(group));
+console.log(group_max);
