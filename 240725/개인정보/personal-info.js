72874d80156788128cf98489bdefa1b007480894
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-25 11:21:01 +0900

    Title: 개인정보, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240725/개인정보/personal-info.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+
+class Student {
+    
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = parseInt(height);
+        this.weight = parseFloat(weight);
+    }
+
+    printInfo(){
+        console.log(`${this.name} ${this.height} ${this.weight}`)
+    }
+}
+
+const studentArr = [];
+
+input.forEach(st => {
+    studentArr.push(new Student(...st.split(" ")));
+});
+
+function sortAndPrint(students){
+
+    function sortByName(){
+        students.sort((a, b) => {
+            if(a.name < b.name) return -1;
+            else if(a.name > b.name) return 1;
+            else return 0;
+        }); 
+    }
+
+    function sortByHeight(){
+        students.sort((a, b) => {
+            if(a.height !== b.height) return b.height - a.height
+        }); 
+    }
+
+    sortByName();
+    console.log("name");
+    students.forEach(student => student.printInfo());
+
+    sortByHeight();
+    console.log();
+    console.log("height");
+    students.forEach(student => student.printInfo());
+}
+
+sortAndPrint(studentArr);
