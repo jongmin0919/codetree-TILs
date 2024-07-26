40be0bbf26848988136fc00ad47178d931f0b6f0
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-26 14:07:41 +0900

    Title: 비오는 날, Time: 206ms, Memory: 14MB, Status: Passed - CodeTree

--- 240726/비오는 날/rainy-day.js
+++ 240726/비오는 날/rainy-day.js
@@ -2,26 +2,30 @@ const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 input.shift();
 
-class weatherInfo{
-    constructor(date, week, weather){
+class weatherInfo {
+    constructor(date, week, weather) {
         this.date = date;
         this.week = week;
         this.weather = weather;
-    };
+    }
 
-    printInfo(){
+    printInfo() {
         console.log(`${this.date} ${this.week} ${this.weather}`);
-    };
-};
+    }
+}
 
 let group = [];
 
-input.forEach(info => {
-    group.push(new weatherInfo(...info.split(" ")));
-})
+for (let i = 0; i < input.length; i++) {
+    let info = input[i];
+    let convertedInfo = info.split(" ");
+    let year = parseInt(convertedInfo[0].split("-")[0]);
+    if (year < 2000 || year > 2100) continue;
+    group.push(new weatherInfo(...convertedInfo));
+}
 
-group.filter(info => info.weather === "Rain").sort((a,b) => {
-    if(a.weather < b.weather) return 1
-    else if (a.weather> b.weather) return -1
-    else return 0
+group.filter(info => info.weather === "Rain").sort((a, b) => {
+    let dateA = new Date(a.date);
+    let dateB = new Date(b.date);
+    return dateA - dateB; 
 })[0].printInfo();
