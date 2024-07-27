74e7435f4665dc207e0c10ed93433f626bc99edf
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-27 22:56:09 +0900

    Title: Date to Date, Time: 226ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240727/Date to Date/date-to-date.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
+let [firstM, firstD, secondM, secondD] = input
+
+const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let accTime = 0;
+
+while(true){
+    if(firstM === secondM && firstD === secondD) break;
+
+    firstD++
+    accTime++;
+
+    if(firstD > num_of_days[firstM]){
+        firstM++;
+        firstD = 1;
+        accTime++;
+    }
+}
+
+console.log(accTime);
