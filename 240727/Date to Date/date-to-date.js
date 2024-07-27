a74485186a54f9b4d724a716ff697039bd0831b9
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-27 22:59:43 +0900

    Title: Date to Date, Time: 240ms, Memory: 15MB, Status: Passed - CodeTree

--- 240727/Date to Date/date-to-date.js
+++ 240727/Date to Date/date-to-date.js
@@ -11,19 +11,6 @@ while (true) {
 
     firstD++;
     accTime++;
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
-let [firstM, firstD, secondM, secondD] = input;
-
-const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-let accTime = 1;
-
-while (true) {
-    if (firstM === secondM && firstD === secondD) break;
-
-
-$0
 
     if (firstD > num_of_days[firstM]) {
         firstM++;
