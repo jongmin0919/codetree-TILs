e4a1bdf1f0ae520380da67398d7de038dbd1a14e
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-27 22:58:55 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240727/Date to Date/date-to-date.js
+++ 240727/Date to Date/date-to-date.js
@@ -1,21 +1,33 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
-let [firstM, firstD, secondM, secondD] = input
+let [firstM, firstD, secondM, secondD] = input;
 
 const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let accTime = 0;
+let accTime = 1;
 
-while(true){
-    if(firstM === secondM && firstD === secondD) break;
+while (true) {
+    if (firstM === secondM && firstD === secondD) break;
 
-    firstD++
+    firstD++;
     accTime++;
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
+let [firstM, firstD, secondM, secondD] = input;
 
-    if(firstD > num_of_days[firstM]){
+const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let accTime = 1;
+
+while (true) {
+    if (firstM === secondM && firstD === secondD) break;
+
+
+$0
+
+    if (firstD > num_of_days[firstM]) {
         firstM++;
         firstD = 1;
-        accTime++;
     }
 }
 
