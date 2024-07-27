ec6c26caa84845e4d4293bd671ad8f3182539e45
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-27 23:11:44 +0900

    Title: DateTime to DateTime, Time: 232ms, Memory: 14MB, Status: Passed - CodeTree

--- 240727/DateTime to DateTime/datetime-to-datetime.js
+++ 240727/DateTime to DateTime/datetime-to-datetime.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
 const [goalD, goalH, goalM] = input;
-let [startD , startH, startM] = [11,11,11];
+let [startD, startH, startM] = [11, 11, 11];
 
 let accMinutes = 0;
 
 while (true) {
+    // 일자가 앞서든지, 시간이 앞서든지, 분이 앞서든지 할 경우 -1를 반환
+    if (startD > goalD || (startD === goalD && startH > goalH) 
+    || (startD === goalD && startH === goalH && startM > goalM)) {
+        accMinutes = -1;
+        break;
+    }
+    
     if (startD === goalD && startH === goalH && startM === goalM) break;
 
     startM++;
     accMinutes++;
 
     if (startM === 60) {
-        startH++
+        startH++;
         startM = 0;
     }
 
