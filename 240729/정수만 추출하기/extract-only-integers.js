b6fe142574fb8f9bcf5f560a62b7cdb92366bf24
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-29 19:24:36 +0900

    Title: 정수만 추출하기, Time: 201ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240729/정수만 추출하기/extract-only-integers.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split(" ");
+
+
+let num1 = "";
+let num2 = "";
+
+for(let i = 0 ; i < input[0].length ; i++){
+    if(input[0][i] > 0 && input[0][i] <= 9) num1 += input[0][i]
+    else break;
+}
+
+for(let i = 0 ; i < input[1].length ; i++){
+    if(input[1][i] > 0 && input[1][i] <= 9) num2 += input[1][i]
+    else break;
+}
+
+console.log(parseInt(num1) + parseInt(num2))
