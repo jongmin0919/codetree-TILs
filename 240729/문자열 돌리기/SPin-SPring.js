fc1eda2839dc285655575d11601464163aeee637
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-29 15:46:28 +0900

    Title: 문자열 돌리기, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240729/문자열 돌리기/SPin-SPring.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const length = input.length;
+
+for (let i = 0; i <= length; i++) {
+    // i를 이용하여 문자열을 오른쪽으로 한 글자씩 밀기
+    let temp = input.slice(length - i) + input.slice(0, length - i);
+    console.log(temp);
+}
