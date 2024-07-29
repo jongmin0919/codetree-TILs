63a124cc0dfbe839053cdee73306fe68f1fc85e3
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-29 11:10:52 +0900

    Title: 문자 제거하기, Time: 233ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240729/문자 제거하기/del-char.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+let word = input.shift().split("");
+
+let idx = 0
+input = input.map(Number)
+
+while(true){
+    if(word.length < input[idx]) word.splice(word.length - 1, 1)
+    else word.splice(input[idx], 1);
+    
+    console.log(word.join(""))
+    if(word.length === 1) break;
+    idx++
+}
