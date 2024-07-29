ace5e7241d03b0092c21a3aa7b838221dec06b7f
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-29 11:13:18 +0900

    Title: 문자 제거하기, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 240729/문자 제거하기/del-char.js
+++ 240729/문자 제거하기/del-char.js
@@ -6,7 +6,7 @@ let idx = 0
 input = input.map(Number)
 
 while(true){
-    if(word.length < input[idx]) word.splice(word.length - 1, 1)
+    if(word.length <= input[idx]) word.splice(word.length - 1, 1)
     else word.splice(input[idx], 1);
     
     console.log(word.join(""))
