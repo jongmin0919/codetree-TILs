f3f930bd3c2ae954f46652b34b8407687d597d07
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-07-29 16:09:13 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 158ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240729/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+let first = input.shift().split(" ");
+let word = first[0]
+const ask = parseInt(first[1])
+const length = word.length;
+
+for(let i = 0 ; i < ask ;i++){
+    let order = Number(input[i])
+    switch(order){
+        case 1:
+        word = word.slice(1) + word.slice(0,1)
+        console.log(word)
+        break;
+        case 2:
+        word = word.slice(length - 1) + word.slice(0, length - 1)
+        console.log(word)
+        break;
+        case 3:
+        let reversed = "";
+        for(let i = length - 1 ; i >= 0 ; i--){
+            reversed += word[i]
+        }
+        console.log(reversed)
+        break;
+    }
+}
