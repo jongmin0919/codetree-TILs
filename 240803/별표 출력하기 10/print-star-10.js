38aa54bba866fba3bc956c340c0c363aa15e6680
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-03 16:23:19 +0900

    Title: 별표 출력하기 10, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240803/별표 출력하기 10/print-star-10.js
+++ 240803/별표 출력하기 10/print-star-10.js
@@ -5,31 +5,27 @@ function printPattern(n) {
     let result = '';
 
     // 위쪽 절반
-    for (let i = 1; i <= 2 * n - 1; i++) {
+    for (let i = 0; i < n; i++) {
         let stars;
-
         if (i % 2 !== 0) {  // 홀수 번째 줄
             stars = n - (i - 1) / 2;
         } else {  // 짝수 번째 줄
             stars = 1 + (i / 2);
         }
-
         result += '* '.repeat(stars).trim() + '\n';
     }
 
-    // 아래쪽 절반 (위쪽 절반을 거꾸로 반복)
-    for (let i = 2 * n - 2; i >= 1; i--) {
+    // 아래쪽 절반
+    for (let i = n - 1; i >= 0; i--) {
         let stars;
-
         if (i % 2 !== 0) {  // 홀수 번째 줄
             stars = n - (i - 1) / 2;
         } else {  // 짝수 번째 줄
             stars = 1 + (i / 2);
         }
-
         result += '* '.repeat(stars).trim() + '\n';
     }
-
+    
     console.log(result);
 }
 
