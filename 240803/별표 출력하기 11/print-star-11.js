77aa835c2160777a72d4e1ea5bf4b5cbd8b81d04
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-03 18:13:17 +0900

    Title: 별표 출력하기 11, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

--- 240803/별표 출력하기 11/print-star-11.js
+++ 240803/별표 출력하기 11/print-star-11.js
@@ -1,18 +1,15 @@
-const fs = require('fs');
-const input = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
+// 변수 선언 및 입력
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
 
 let str = "";
 
-for (let i = 0; i < input*2+1; i++) {
+// 2 * n + 1칸의 정사각형에서 i, j에 짝수가 들어가면 *을 출력합니다.
+for (let i = 0; i < 2 * n + 1; i++) {
     str = "";
-    for (let j = 0; j <= input*4 + 1; j++) {
-
-        if ((i%2 === 0 && j%2 === 0) || j%(input + 1) === 0) {
-            str += "*";
-        }
-        else {
-            str += " ";
-        }
+    for (let j = 0; j < 2 * n + 1; j++) {
+        if (i % 2 == 0 || j % 2 == 0) str += "* ";
+        else str += "  ";
     }
     console.log(str);
 }
