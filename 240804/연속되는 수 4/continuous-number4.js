43edd6b93c749e22a8b54f2306b64f32b7ac4f8e
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-04 21:44:23 +0900

    Title: 연속되는 수 4, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240804/연속되는 수 4/continuous-number4.js
+++ 240804/연속되는 수 4/continuous-number4.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
-
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n").map(Number);
 
 let cnt = 1;
 let max = 0;
-for(let i = 0 ; i < input.length-2 ; i++){
+
+for(let i = 0 ; i < input.length - 1 ; i++){
     if(input[i] < input[i + 1]){
         cnt++;
         if(max < cnt) max = cnt;
-    }else cnt = 1;
+    } else {
+        cnt = 1;
+    }
 }
 
-console.log(max === 0 ? 1 : max)
+console.log(max === 0 ? 1 : max);
