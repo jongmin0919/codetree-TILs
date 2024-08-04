aa020cfe06ce31976df52b21dceea1ce6bbde68d
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-04 21:37:55 +0900

    Title: 연속되는 수 3, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240804/연속되는 수 3/continuous-number3.js
+++ 240804/연속되는 수 3/continuous-number3.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 
 let cnt = 1;
 let max = 0;
-for(let i = 0 ; i < input.length-1 ; i++){
+for(let i = 0 ; i < input.length-2 ; i++){
     if((input[i] > 0 && input[i + 1] > 0) || (input[i] < 0 && input[i + 1] < 0)){
         cnt++;
         if(max < cnt) max = cnt;
