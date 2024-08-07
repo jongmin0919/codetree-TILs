73489a4abcaea3b0fe3ba905958d1f7f24d7f0da
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-08 08:51:18 +0900

    Title: 약수가 세 개인 수, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240808/약수가 세 개인 수/numbers-with-three-divisors.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
+const [start, end] = [...input]
+
+let cnt = 0;
+for(let i = start ; i <= end ; i++){
+    let tempArr = [];
+    for(let j = 1 ; j < i ; j++){
+        if(i%j === 0) tempArr.push(j)
+    }
+    if(tempArr.reduce((acc, cur) => acc + cur, 0) === 3) cnt++;
+}
+console.log(cnt);
