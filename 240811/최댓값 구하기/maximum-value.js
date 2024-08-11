06bf396316400132e45af1638273a48d42b4ea80
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-11 22:08:48 +0900

    Title: 최댓값 구하기, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 240811/최댓값 구하기/maximum-value.js
+++ 240811/최댓값 구하기/maximum-value.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
-let [a,b,c] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let [a, b, c] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-let max = 0;
+let max = a;
 
-if(a !== b && a !== c){
-    if(a > b && a > c) max = a;
-    else if(b > a && b > c) max = b;
-    else max = c;
-}else if((a === b && a !== c) || (a !== b && a === c)){
-    if(a > c ) max = a;
-    else max = c;
-}else max = a;
+
+if (b > max) {
+    max = b;
+}
+
+
+if (c > max) {
+    max = c;
+}
 
 console.log(max);
