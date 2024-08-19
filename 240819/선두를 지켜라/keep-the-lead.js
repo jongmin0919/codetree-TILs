b431d981dda26ba995fb2b4caa0e52c1d660a896
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-19 17:58:38 +0900

    Title: 선두를 지켜라, Time: 277ms, Memory: 17MB, Status: Passed - CodeTree

--- 240819/선두를 지켜라/keep-the-lead.js
+++ 240819/선두를 지켜라/keep-the-lead.js
@@ -3,20 +3,23 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
 const runner = input.shift().split(" ").map(Number);
 const aMoving = input.splice(0,runner[0]);
 const bMoving = input.splice(0);
+let totalTime = 0;
+aMoving.forEach(item => {
+    totalTime += Number(item.split(" ")[1])
+})
 
-
+// 첫 번째 입력 값 활용할 수 있게 초기값 설정 변경
 let aCnt = bCnt = aPer = bPer = 0;
-let firstLine = aMoving.shift().split(" ").map(Number);
-let secondLine = bMoving.shift().split(" ").map(Number);
-let [aVelocity, bVelocity, aTime, bTime]  = [firstLine.shift(), firstLine.shift(), secondLine.shift(), secondLine.shift()]
+// let firstLine = aMoving.shift().split(" ").map(Number);
+// let secondLine = bMoving.shift().split(" ").map(Number);
+let aVelocity = bVelocity = aTime = bTime = 0
+//[firstLine.shift(), firstLine.shift(), secondLine.shift(), secondLine.shift()]
 
 let preReader = curReader = "";
 
-
 let result = 0;
 
-while (aMoving.length > 0 || bMoving.length > 0) {
-
+for(let i = 0 ; i < totalTime ; i++){
     if(aMoving.length > 0 && aCnt === aTime){
         let Line = aMoving.shift().split(" ").map(Number);
         [aVelocity, aTime] = Line;
@@ -29,9 +32,12 @@ while (aMoving.length > 0 || bMoving.length > 0) {
         bCnt = 0;
     }
 
-    aPer += aVelocity/aTime;
-    bPer += bVelocity/bTime;
+    // 이동 거리 v를 주어진 t번만큼 더해주기
+    aPer += aVelocity;
+    bPer += bVelocity;
     aCnt++, bCnt++;
+    //console.log(aPer, "|",  bPer);
+
 
     if (aPer > bPer) curReader = "A";
     else if (bPer > aPer) curReader = "B";
@@ -43,4 +49,6 @@ while (aMoving.length > 0 || bMoving.length > 0) {
     }
 }
 
-console.log(result);
+// 첫 번째 추월 카운팅 한 거 빼주기
+if(result > 0) result -= 1;
+console.log(result)
