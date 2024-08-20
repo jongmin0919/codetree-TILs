e13798203d7ff12790db9cdb50e34406541e958b
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-20 23:31:43 +0900

    Title: 되돌아오기, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 240820/되돌아오기/come-back.js
+++ 240820/되돌아오기/come-back.js
@@ -2,29 +2,34 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 // 입력 처리
-const N = parseInt(input[0]);
+const n = parseInt(input[0]);
 const commands = input.slice(1).map(line => line.split(' '));
 
-// 방향 매핑
+// 동서남북 방향 매핑
 const directionMap = {
-    'N': [0, 1],
-    'S': [0, -1],
-    'E': [1, 0],
-    'W': [-1, 0]
+    'N': [0, 1],   
+    'S': [0, -1],  
+    'E': [1, 0],   
+    'W': [-1, 0]   
 };
 
 // 현재 위치 및 시간
 let x = 0, y = 0;
 let time = 0;
 
-// 이동 처리
-for (const [direction, distance] of commands) {
+// 각 명령을 처리
+for (let [direction, distance] of commands) {
+    // 현재 좌표의 방향으로 dx와 dy를 설정
     const [dx, dy] = directionMap[direction];
-    for (let step = 0; step < parseInt(distance); step++) {
+    distance = parseInt(distance);
+    
+    // 거리만큼 반복문을 돌면서 x와 y를 방향대로 이동
+    for (let step = 0; step < distance; step++) {
         x += dx;
         y += dy;
         time += 1;
-        // 시작점으로 돌아온 경우
+
+        // 시작점으로 돌아온 경우 누적된 시간을 출력하고 해당 함수 종료
         if (x === 0 && y === 0) {
             console.log(time);
             return;
@@ -32,5 +37,5 @@ for (const [direction, distance] of commands) {
     }
 }
 
-// 시작점으로 돌아오지 못한 경우
+// 시작점으로 돌아오지 못한 -1를 출력
 console.log(-1);
