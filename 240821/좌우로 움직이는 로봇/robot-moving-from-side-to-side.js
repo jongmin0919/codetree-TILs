d2170fb4630a2d5966b6411cf05796978ec7d41c
다시가리(Re_Go) <144569961+jongmin0919@users.noreply.github.com>
2024-08-21 14:42:38 +0900

    Title: 좌우로 움직이는 로봇, Time: 413ms, Memory: 69MB, Status: Wrong Answer - CodeTree

--- 240821/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
+++ 240821/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -5,12 +5,8 @@ const aMoving = input.splice(0, runner[0]);
 const bMoving = input.splice(0);
 
 // 각 A와 B의 배열
-let arrA = [];
-let arrB = [];
-
-// A와 B 배열의 첫번째 부분에는 0을 집어넣음 (둘 다 0에서 출발하므로)
-arrA.push(0);
-arrB.push(0);
+let arrA = [0];
+let arrB = [0];
 
 // A와 B 각각의 주어지는 라인과 시간, 위치를 초기화
 let aIndex = 0, bIndex = 0;
@@ -22,17 +18,20 @@ let result = 0;
 
 while (aIndex < aMoving.length || bIndex < bMoving.length || aSecond > 0 || bSecond > 0) {
     if (aIndex < aMoving.length && aSecond === 0) {
-        let aLine = aMoving[aIndex].split(" ");
-        [aSecond, aOrder] = [parseInt(aLine[0]), aLine[1]];
+        let [second, order] = aMoving[aIndex].split(" ");
+        aSecond = parseInt(second);
+        aOrder = order;
         aIndex++;
     }
     
     if (bIndex < bMoving.length && bSecond === 0) {
-        let bLine = bMoving[bIndex].split(" ");
-        [bSecond, bOrder] = [parseInt(bLine[0]), bLine[1]];
+        let [second, order] = bMoving[bIndex].split(" ");
+        bSecond = parseInt(second);
+        bOrder = order;
         bIndex++;
     }
-     
+    
+    // count는 aSecond와 bSecond 중 큰 값으로 설정
     let count = Math.max(aSecond, bSecond);
     
     for (let i = 0; i < count; i++) {
@@ -44,7 +43,10 @@ while (aIndex < aMoving.length || bIndex < bMoving.length || aSecond > 0 || bSec
         arrA.push(aPosition);
         arrB.push(bPosition);
 
-        if (arrA[arrA.length - 1] === arrB[arrB.length - 1] && arrA[arrA.length - 2] !== arrB[arrB.length - 2]) result++;
+        // 마지막 두 요소 비교
+        if (arrA[arrA.length - 1] === arrB[arrB.length - 1] && arrA[arrA.length - 2] !== arrB[arrB.length - 2]) {
+            result++;
+        }
         
         if (aSecond > 0) aSecond--;
         if (bSecond > 0) bSecond--;
